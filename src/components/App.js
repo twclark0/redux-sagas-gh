993ffac54c2a730c9623b5d9ee2e1d960aaa9be7
twclark0 <tylerc644@gmail.com>
2017-10-19 00:46:29 -0600

    Add basic CSS and key to map function

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
+import './App.css'
+
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Redux Sagas</h1>
         <div>
-          {/*{this.props.starWars.people.map(person => <h4>{person.name}</h4>)}*/}
+          {/*{this.props.starWars.people.map((person, i) => <h4 key={i}>{person.name}</h4>)}*/}
         </div>
         <button onClick={this.props.fetchStarWarsRequest}>Load More</button>
       </div>
