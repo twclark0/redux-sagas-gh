a71308344f5ebd9418b157f6ebc365ac2e20d053
twclark0 <tylerc644@gmail.com>
2017-10-29 22:16:08 -0600

    update saga name

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Redux Sagas</h1>
+        <h1>Redux Saga</h1>
         <div>
           {this.props.starWars.people.map((person, i) => <h4 key={i}>{person.name}</h4>)}
         </div>
